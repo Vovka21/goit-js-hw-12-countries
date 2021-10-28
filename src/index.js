ac61b5c4824946aa90f2f988db8a3f1e431cad30
Вовка <vavanich90@gmail.com>
2021-10-28 19:20:56 +0300

    finished hw12

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import '@pnotify/core/dist/BrightTheme.css';
+
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+const _ = require('lodash');
+
+// var debounce = require('debounce');
+// import { debounce } from 'debounce';
+
 import countryCardTpl from './country-card.hbs';
 
 const cardContainer = document.querySelector('.js-card-container');
@@ -9,22 +19,67 @@ function fetchCountry(name) {
   });
 }
 
-function xz(country) {
-  const markup = countryCardTpl(country);
-
-  console.log(markup);
-
-  cardContainer.innerHTML = markup;
+function delAlert() {
+  const pnotify = document.querySelector('.pnotify');
+  pnotify.style.opacity = 0;
 }
 
-input.addEventListener('input', onSearch);
+let ul = document.createElement('ul');
+cardContainer.append(ul);
+
+function addCountry(country) {
+  const markup = countryCardTpl(country);
+
+  if (country.length === 1) {
+    delAlert();
+    cardContainer.innerHTML = '';
+    ul.innerHTML = '';
+
+    cardContainer.innerHTML = markup;
+  }
+
+  if (country.length > 10) {
+    cardContainer.innerHTML = '';
+
+    defaultModules.set(PNotifyMobile, {});
+
+    alert({
+      text: 'Слишком много хочешь!',
+    });
+  }
+
+  if (country.length > 1 && country.length < 10) {
+    cardContainer.innerHTML = '';
+    delAlert();
+
+    cardContainer.append(ul);
+
+    const listItemsMarkup = createListItemsMarkup(country);
+    ul.innerHTML = listItemsMarkup;
+
+    function createListItemsMarkup(items) {
+      return items.map(item => `<li>${item.name}</li>`).join('');
+    }
+
+    // function onFilterChange(evt) {
+    // const filter = evt.target.value.toLowerCase();
+    //   const filteredItems = country.filter(t => t.name.toLowerCase().includes(filter));
+    //   const listItemsMarkup = createListItemsMarkup(filteredItems);
+    //   ul.innerHTML = listItemsMarkup;
+    // }
+  }
+}
+
+input.addEventListener('input', _.debounce(onSearch, 500));
 
 function onSearch(e) {
-  e.preventDefault();
+  let searchQuery = input.value;
 
-  const searchQuery = input.value;
-
-  fetchCountry(searchQuery)
-    .then(xz)
-    .catch(error => console.log('error'));
+  if (searchQuery.length === 0) {
+    cardContainer.innerHTML = '';
+  } else {
+    fetchCountry(searchQuery)
+      .then(addCountry)
+      .catch(error => console.log('error'));
+  }
 }
